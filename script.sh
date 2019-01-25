1f4a950990a1148ffe5a3584068eddda6a2b34d5
jen lisabeth <lisabeth.jen@hotmail.com>
2019-01-25 07:06:05 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ sudo -i
 rm cityconfig -R -f
 git clone https://github.com/jenlisabeth/template-testing cityconfig
 cd cityconfig
-./scripts/install.sh $1 $2 $3 $4 $5 $6 $7
+sudo sh ./scripts/install.sh $1 $2 $3 $4 $5 $6 $7
